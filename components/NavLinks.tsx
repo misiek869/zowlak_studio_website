d8fe8c2804eebdf97907c170a26e6cc89ec992d7
Michal Gawik <gawlik.zator@o2.pl>
2025-05-16 09:41:06 +0200

    style links and add underline

--- components/NavLinks.tsx
+++ components/NavLinks.tsx
@@ -1,4 +1,7 @@
+'use client'
+
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React from 'react'
 
 type LinksArr = {
@@ -14,12 +17,26 @@ const links: LinksArr[] = [
 ]
 
 const NavLinks = ({ containerStyles }: { containerStyles: string }) => {
+	const pathname = usePathname()
+
 	return (
 		<ul className={containerStyles}>
 			{links.map((link, index) => {
+				const isActiveRoute = pathname === link.path
+
+				const charLength = link.name.length
+
+				const lineWidth = charLength > 5 ? 'after:w-[120%]' : 'after:w-[90%]'
+				console.log(charLength)
 				return (
-					<Link key={index} href={link.path}>
-						{link.name}
+					<Link
+						className={`relative text-lg uppercase text-slate-50 ${
+							isActiveRoute &&
+							`after:content-[''] after:block after:absolute after:left-0 after:bottom-[-1] ${lineWidth} after:h-[4px] after:bg-slate-50 after:z-0`
+						}`}
+						key={index}
+						href={link.path}>
+						<span className='relative z-10'>{link.name}</span>
 					</Link>
 				)
 			})}
