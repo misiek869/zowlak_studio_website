8d0e9f19fe16e21dd499df5e9e45599bfa0e5c1f
Michal Gawik <gawlik.zator@o2.pl>
2025-05-16 09:51:49 +0200

    setup motion div

--- components/PageTransition.tsx
+++ components/PageTransition.tsx
@@ -1,11 +1,28 @@
-import React from 'react'
+'use client'
+
+import { AnimatePresence, motion } from 'framer-motion'
+import { usePathname } from 'next/navigation'
 
 const PageTransition = ({
 	children,
 }: Readonly<{
 	children: React.ReactNode
 }>) => {
-	return <div>{children}</div>
+	const pathname = usePathname()
+
+	return (
+		<AnimatePresence>
+			<motion.div
+				className='min-h-screen w-screen fixed top-0 pointer-events-none'
+				initial={{ opacity: 1 }}
+				animate={{
+					opacity: 0,
+					transition: { delay: 1, duration: 0.4, ease: 'easeInOut' },
+				}}
+			/>
+			<div key={pathname}>{children}</div>
+		</AnimatePresence>
+	)
 }
 
 export default PageTransition
