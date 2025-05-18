c790e3e494533f2c91dd800d5d603a11468706ce
Michal Gawik <gawlik.zator@o2.pl>
2025-05-18 07:29:21 +0200

    create first Home page draft

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,16 @@ import { motion } from 'framer-motion'
 const Home = () => {
 	return (
 		<motion.section
+			className='flex h-screen items-center'
 			initial={{ opacity: 0 }}
 			animate={{
 				opacity: 1,
 				transition: { delay: 1, duration: 0.4, ease: 'easeIn' },
 			}}>
-			home
+			<div className='flex flex-col xl:flex-row items-center justify-between w-full'>
+				<div className='w-full xl:w-[550px]'>text</div>
+				<div className='flex-1 hidden xl:block relative z-20'>{/* blob */}</div>
+			</div>
 		</motion.section>
 	)
 }
