043e8e5ebd80f0adb5a1c2359a445124a25fe5cd
Michal Gawik <gawlik.zator@o2.pl>
2025-05-20 13:42:06 +0200

    react-type-animation

--- app/page.tsx
+++ app/page.tsx
@@ -14,10 +14,13 @@ const Home = () => {
 				transition: { delay: 1, duration: 0.4, ease: 'easeIn' },
 			}}>
 			<div className='flex flex-col xl:flex-row items-center justify-between w-full'>
-				<div className='w-full xl:w-[550px]'>text</div>
+				<div className='w-full xl:w-[550px]'
+				
+				</div>
+
 				<div className='flex-1 hidden xl:block relative z-20'>
 					<Blob containerStyles={'w-[560px] h-[560px]'} />
-
+					{/* 
 					<Image
 						src={avatarImage}
 						alt='avatar image'
@@ -25,7 +28,7 @@ const Home = () => {
 						height={600}
 						quality={100}
 						className='absolute -top-1 left-[110px]'
-					/>
+					/> */}
 
 					<div className='w-full h-[164px]'></div>
 				</div>
