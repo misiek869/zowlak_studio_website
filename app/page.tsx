59c0b74c41c60a972a9bcdeee0ddaf149d69f3b5
Michal Gawik <gawlik.zator@o2.pl>
2025-05-20 14:09:47 +0200

    create TypeAnimation sequence

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import Blob from '@/components/Blob'
 import { motion } from 'framer-motion'
+import { TypeAnimation } from 'react-type-animation'
 import Image from 'next/image'
 import avatarImage from '@/public/assets/avatar.png'
 
@@ -14,8 +15,40 @@ const Home = () => {
 				transition: { delay: 1, duration: 0.4, ease: 'easeIn' },
 			}}>
 			<div className='flex flex-col xl:flex-row items-center justify-between w-full'>
-				<div className='w-full xl:w-[550px]'
-				
+				<div className='w-full xl:w-[550px]'>
+					<h1 className='h1'>
+						<TypeAnimation
+							sequence={[
+								'Z',
+								400,
+								'Zw',
+								200,
+								'Zwo',
+								200,
+								'Zwol',
+								200,
+								'Zwola',
+								200,
+								'Zwolak',
+								200,
+								'Zwolak S',
+								200,
+								'Zwolak St',
+								200,
+								'Zwolak Stu',
+								200,
+								'Zwolak Stud',
+								200,
+								'Zwolak Studio',
+								4000,
+							]}
+							wrapper={'h1'}
+							speed={60}
+							className=''
+							// repeat={Infinity}
+							cursor={false}
+						/>
+					</h1>
 				</div>
 
 				<div className='flex-1 hidden xl:block relative z-20'>
