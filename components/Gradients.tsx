037479ab80b6fea2f8fb8f8df9612a7b308d70aa
Michal Gawik <gawlik.zator@o2.pl>
2025-05-25 11:09:44 +0200

    add absolute position

--- components/Gradients.tsx
+++ components/Gradients.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Gradients = () => {
 	return (
-		<div className='hidden md:block w-[480px] h-[480px] rounded-full bg-gray-700 -top-[30%] -right-[4%] z-30 blur-[300px]'></div>
+		<div className='hidden md:block absolute w-[480px] h-[480px] rounded-full bg-gray-700 -top-[30%] -right-[4%] z-30 blur-[300px]' />
 	)
 }
 
