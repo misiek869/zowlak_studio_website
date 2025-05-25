2130d3f93c76ec6c4dd568512c1de69a744a145d
Michal Gawik <gawlik.zator@o2.pl>
2025-05-25 11:06:12 +0200

    create Gradient component

---
+++ components/Gradients.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Gradients = () => {
+	return (
+		<div className='hidden md:block w-[480px] h-[480px] rounded-full bg-gray-700 -top-[30%] -right-[4%] z-30 blur-[300px]'></div>
+	)
+}
+
+export default Gradients
