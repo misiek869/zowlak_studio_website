18e21c7ad6238312a45805cc829817ba22d8e12b
Michal Gawik <gawlik.zator@o2.pl>
2025-05-28 05:31:48 +0200

    change styles and add Logo

--- components/MainNav.tsx
+++ components/MainNav.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import NavLinks from './NavLinks'
+import Logo from './Logo'
 
 const MainNav = () => {
 	return (
-		<nav>
-			<NavLinks containerStyles=' flex flex-col gap-6 ' />
+		<nav className='w-full py-12 px-8'>
+			<div className='flex flex-col h-full items-baseline'>
+				<Logo />
+				<NavLinks containerStyles='flex flex-col gap-6 mt-20' linkStyle='' />
+			</div>
 		</nav>
 	)
 }
