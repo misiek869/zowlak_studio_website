74d25a2fec45fdf9d1cca98e7f95204af914f029
Michal Gawik <gawlik.zator@o2.pl>
2025-05-29 05:26:20 +0200

    create first CountUp

--- components/Stats.tsx
+++ components/Stats.tsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import CountUp from 'react-countup'
 
 const Stats = () => {
-	return <div>stats</div>
+	return (
+		<div>
+			<div className='flex gap-4'>
+				<div className='text-5xl font-bold text-gray-900 flex'>
+					<CountUp end={12} duration={5} delay={2} className='block w-[60px]' />
+					<div className=''>+</div>
+				</div>
+
+				<span className='font-semibold'>
+					Lat <br />
+					Doświadczenia
+				</span>
+			</div>
+		</div>
+	)
 }
 
 export default Stats
