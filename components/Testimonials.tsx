3434e606a1a413e28a51c4f18415f9026c228cb7
Michal Gawik <gawlik.zator@o2.pl>
2025-05-29 05:51:23 +0200

    create first testiomonials markup

--- components/Testimonials.tsx
+++ components/Testimonials.tsx
@@ -1,7 +1,47 @@
 import React from 'react'
 
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Autoplay } from 'swiper/modules'
+import 'swiper/css'
+import 'swiper/css/pagination'
+
+import { ImQuotesLeft } from 'react-icons/im'
+
+type Testimonial = {
+	message: string
+	name: string
+}
+
+const testimonials: Testimonial[] = [
+	{
+		message:
+			'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex, adipisci.',
+		name: 'John Doe',
+	},
+	{
+		message:
+			'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex, adipisci.',
+		name: 'John Doe',
+	},
+	{
+		message:
+			'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex, adipisci.',
+		name: 'John Doe',
+	},
+]
+
 const Testimonials = () => {
-	return <div>Testimonials</div>
+	return (
+		<Swiper
+			modules={[Autoplay]}
+			loop={true}
+			autoplay={{ delay: 4000, disableOnInteraction: false }}
+			className='w-full max-w-[310px] md:max-w-[520px] bg-red-200 rounded-lg'>
+			{testimonials.map((item, index) => {
+				return <SwiperSlide key={index}>{item.message}</SwiperSlide>
+			})}
+		</Swiper>
+	)
 }
 
 export default Testimonials
