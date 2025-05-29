794c17440914f1a5e0662393b2af1b79139fc92b
Michal Gawik <gawlik.zator@o2.pl>
2025-05-29 05:33:11 +0200

    create second CountUp

--- components/Stats.tsx
+++ components/Stats.tsx
@@ -2,7 +2,7 @@ import CountUp from 'react-countup'
 
 const Stats = () => {
 	return (
-		<div>
+		<div className=' flex flex-col xl:flex-row xl:items-center gap-8 xl:gap-12'>
 			<div className='flex gap-4'>
 				<div className='text-5xl font-bold text-gray-900 flex'>
 					<CountUp end={12} duration={5} delay={2} className='block w-[60px]' />
@@ -12,6 +12,18 @@ const Stats = () => {
 				<span className='font-semibold'>
 					Lat <br />
 					Doświadczenia
+				</span>
+			</div>
+
+			<div className='flex gap-4'>
+				<div className='text-5xl font-bold text-gray-900 flex'>
+					<CountUp end={55} duration={5} delay={2} className='block w-[60px]' />
+					<div className=''>+</div>
+				</div>
+
+				<span className='font-semibold'>
+					Zrealizowanych <br />
+					Projektów
 				</span>
 			</div>
 		</div>
