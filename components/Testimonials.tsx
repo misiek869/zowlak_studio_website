e3104d684c48fd6ed818bc5958bd112a9daa632f
Michal Gawik <gawlik.zator@o2.pl>
2025-05-30 05:41:22 +0200

    finish testimonials swiper

--- components/Testimonials.tsx
+++ components/Testimonials.tsx
@@ -5,7 +5,7 @@ import { Autoplay } from 'swiper/modules'
 import 'swiper/css'
 import 'swiper/css/pagination'
 
-import { ImQuotesLeft } from 'react-icons/im'
+import { FaRegCommentDots } from 'react-icons/fa'
 
 type Testimonial = {
 	message: string
@@ -36,9 +36,19 @@ const Testimonials = () => {
 			modules={[Autoplay]}
 			loop={true}
 			autoplay={{ delay: 4000, disableOnInteraction: false }}
-			className='w-full max-w-[310px] md:max-w-[520px] bg-red-200 rounded-lg'>
+			className='w-full max-w-[300px] md:max-w-[520px] bg-red-200 rounded-lg'>
 			{testimonials.map((item, index) => {
-				return <SwiperSlide key={index}>{item.message}</SwiperSlide>
+				return (
+					<SwiperSlide key={index}>
+						<div className='flex px-8 py-6 gap-8'>
+							<FaRegCommentDots className='hidden xl:flex text-8xl text-gray-900' />
+							<div className='flex flex-col gap-2'>
+								<p>{item.message}</p>
+								<p className='self-end font-semibold'>{item.name}</p>
+							</div>
+						</div>
+					</SwiperSlide>
+				)
 			})}
 		</Swiper>
 	)
