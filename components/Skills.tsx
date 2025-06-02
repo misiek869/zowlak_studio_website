11f45a2873bd97914fe3eeb4579b1e915adf6ebb
Michal Gawik <gawlik.zator@o2.pl>
2025-06-02 05:13:35 +0200

    install shadcn tooltip

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,4 +1,11 @@
-import React from 'react'
+import {
+	RiReactjsFill,
+	RiNextjsFill,
+	RiHtml5Fill,
+	RiCss3Fill,
+	RiTailwindCssFill,
+	RiNodejsFill,
+} from 'react-icons/ri'
 
 const Skills = () => {
 	return <div></div>
