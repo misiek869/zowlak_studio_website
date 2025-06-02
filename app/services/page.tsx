cbb118b6ac2f48bb4cdecc192331435ac7033f54
Michal Gawik <gawlik.zator@o2.pl>
2025-06-02 06:12:55 +0200

    first page  work

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,7 +1,53 @@
-import React from 'react'
+'use client'
+import { motion } from 'framer-motion'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Pagination } from 'swiper/modules'
+import 'swiper/css'
+import 'swiper/css/pagination'
+import Image from 'next/image'
+import { MdOutlineArrowOutward, MdArrowRightAlt } from 'react-icons/md'
+import { title } from 'process'
+
+type Service = {
+	icon: string
+	href: string
+	title: string
+}
+
+const services: Service[] = [
+	{
+		icon: '/services/design.svg',
+		href: '',
+		title: 'Random Title',
+	},
+	{
+		icon: '/services/design.svg',
+		href: '',
+		title: 'Random Title',
+	},
+	{
+		icon: '/services/design.svg',
+		href: '',
+		title: 'Random Title',
+	},
+	{
+		icon: '/services/design.svg',
+		href: '',
+		title: 'Random Title',
+	},
+]
 
 const ServicesPage = () => {
-	return <div></div>
+	return (
+		<motion.section
+			initial={{ opacity: 0 }}
+			animate={{
+				opacity: 1,
+				transition: { delay: 2.4, duration: 0.4, ease: 'easeIn' },
+			}}>
+			contact
+		</motion.section>
+	)
 }
 
 export default ServicesPage
