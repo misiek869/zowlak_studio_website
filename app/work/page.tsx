d713542cb1e17efa0d9a78e47b063fa26ef9fdd5
Michal Gawik <gawlik.zator@o2.pl>
2025-06-03 06:12:47 +0200

    add imports and create projects array

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,5 +1,62 @@
 'use client'
 import { motion } from 'framer-motion'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Pagination } from 'swiper/modules'
+import 'swiper/css'
+import 'swiper/css/pagination'
+
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+import Link from 'next/link'
+import Image from 'next/image'
+import { MdOutlineArrowOutward } from 'react-icons/md'
+import { FaGithub } from 'react-icons/fa'
+
+const projects = [
+	{
+		id: 1,
+		category: 'budowlane',
+		title: 'Projekt 1',
+		description:
+			'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, asperiores.',
+		image: '/assets/work/work.jpg',
+		link: '',
+		github: '',
+		tech: ['react', 'typescript'],
+	},
+	{
+		id: 2,
+		category: 'budowlane',
+		title: 'Projekt 1',
+		description:
+			'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, asperiores.',
+		image: '/assets/work/work.jpg',
+		link: '',
+		github: '',
+		tech: ['react', 'typescript'],
+	},
+	{
+		id: 3,
+		category: 'budowlane',
+		title: 'Projekt 1',
+		description:
+			'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, asperiores.',
+		image: '/assets/work/work.jpg',
+		link: '',
+		github: '',
+		tech: ['react', 'typescript'],
+	},
+	{
+		id: 4,
+		category: 'budowlane',
+		title: 'Projekt 1',
+		description:
+			'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, asperiores.',
+		image: '/assets/work/work.jpg',
+		link: '',
+		github: '',
+		tech: ['react', 'typescript'],
+	},
+]
 
 const WorkPage = () => {
 	return (
