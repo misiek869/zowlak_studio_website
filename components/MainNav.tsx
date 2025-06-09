648c22947784938a25ea657a6d7e20af7e6230dd
Michal Gawik <gawlik.zator@o2.pl>
2025-06-09 12:49:49 +0200

    move social links to main nav

--- components/MainNav.tsx
+++ components/MainNav.tsx
@@ -5,12 +5,13 @@ import Socials from './Socials'
 
 const MainNav = () => {
 	return (
-		<nav className='w-full py-12 px-8 '>
+		<nav className='w-full py-12 px-8 relative'>
 			<div className='flex flex-col h-full items-baseline'>
 				<Logo />
 				<NavLinks containerStyles='flex flex-col gap-6 mt-20' linkStyle='' />
 			</div>
-			<div className='hidden'>
+			{/* Div z socialami na dole, wyśrodkowany poziomo */}
+			<div className='absolute bottom-8 left-1/2 transform -translate-x-1/2'>
 				<Socials
 					containerStyles='flex gap-8'
 					iconStyles='w-[48px] h-[48px] text-[26px] hover:text-slate-50 hover:bg-gray-900 transition-all duration-300 flex items-center justify-center rounded-full cursor-pointer bg-slate-50 text-gray-900'
