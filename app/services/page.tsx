fe8d6fab7612cb4e23f75a7822fdd2820124165c
Michal Gawik <gawlik.zator@o2.pl>
2025-06-12 05:38:25 +0200

    add section with interior projects

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -14,7 +14,30 @@ type Service = {
 	title: string
 }
 
-const services: Service[] = [
+const exteriorServices: Service[] = [
+	{
+		icon: '/assets/services/design.svg',
+		href: '',
+		title: 'Projekty Typowe',
+	},
+	{
+		icon: '/assets/services/design.svg',
+		href: '',
+		title: 'Projekty Indywidualne',
+	},
+	{
+		icon: '/assets/services/design.svg',
+		href: '',
+		title: 'Zmiana sposobu użytkowania',
+	},
+	{
+		icon: '/assets/services/design.svg',
+		href: '',
+		title: 'Random Title',
+	},
+]
+
+const interiorServices: Service[] = [
 	{
 		icon: '/assets/services/design.svg',
 		href: '',
@@ -52,7 +75,7 @@ const ServicesPage = () => {
 						Projekty <span className='font-bold'>Budowlane</span>
 					</h2>
 					<button className='btn btn-lg btn-gray flex gap-2'>
-						Zobacz moje projekty <MdArrowRightAlt className='text-2xl' />
+						Zobacz projekty budowlanee <MdArrowRightAlt className='text-2xl' />
 					</button>
 				</div>
 				<Swiper
@@ -65,7 +88,7 @@ const ServicesPage = () => {
 					}}
 					modules={[Pagination]}
 					pagination={{ clickable: true, dynamicBullets: true }}>
-					{services.map((item, index) => {
+					{exteriorServices.map((item, index) => {
 						return (
 							<SwiperSlide key={index}>
 								<div className='bg-gray-900/90 w-full h-[284px] rounded-xl px-[30px] py-[40px] flex flex-col justify-between'>
@@ -80,7 +103,50 @@ const ServicesPage = () => {
 											<MdOutlineArrowOutward />
 										</div>
 									</div>
-									<h5 className='text-[22px] text-slate-50 font-medium max-w-[240px]'>
+									<h5 className='text-2xl text-slate-50 font-medium max-w-[240px] text-center my-auto'>
+										{item.title}
+									</h5>
+								</div>
+							</SwiperSlide>
+						)
+					})}
+				</Swiper>
+
+				<div className='flex flex-col xl:flex-row justify-between items-start xl:items-center gap-8'>
+					<h2 className='h2 max-w-[480px] text-left xl:mb-0'>
+						Projekty <span className='font-bold'>Wnętrz</span>
+					</h2>
+					<button className='btn btn-lg btn-gray flex gap-2'>
+						Zobacz projekty wnętrz
+						<MdArrowRightAlt className='text-2xl' />
+					</button>
+				</div>
+				<Swiper
+					className='h-[320px]'
+					spaceBetween={30}
+					slidesPerView={1}
+					breakpoints={{
+						640: { slidesPerView: 2 },
+						1024: { slidesPerView: 3 },
+					}}
+					modules={[Pagination]}
+					pagination={{ clickable: true, dynamicBullets: true }}>
+					{interiorServices.map((item, index) => {
+						return (
+							<SwiperSlide key={index}>
+								<div className='bg-gray-900/90 w-full h-[284px] rounded-xl px-[30px] py-[40px] flex flex-col justify-between'>
+									<div className=' flex items-center justify-between mb-12'>
+										<Image
+											src={item.icon}
+											width={48}
+											height={48}
+											alt='service'
+										/>
+										<div className='w-12 h-12 bg-gray-900 rounded-full flex items-center justify-center cursor-pointer text-2xl hover:rotate-45 transition-all text-slate-50'>
+											<MdOutlineArrowOutward />
+										</div>
+									</div>
+									<h5 className='text-2xl text-slate-50 font-medium max-w-[240px] text-center my-auto'>
 										{item.title}
 									</h5>
 								</div>
