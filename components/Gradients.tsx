a2c541a8bd0f9a007c4f38d5826f30befb2963eb
Michal Gawik <gawlik.zator@o2.pl>
2025-06-13 05:42:47 +0200

    change gradient color

--- components/Gradients.tsx
+++ components/Gradients.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 const TopGradient = () => {
 	return (
-		<div className='hidden md:block absolute w-[480px] h-[480px] rounded-full bg-gray-700 -top-[40%] -right-[4%] z-30 blur-[300px]' />
+		<div className='hidden md:block absolute w-[480px] h-[480px] rounded-full bg-gray-900 -top-[40%] -right-[4%] z-30 blur-[300px]' />
 	)
 }
 
 const BottomGradient = () => {
 	return (
-		<div className='hidden md:block absolute w-[480px] h-[480px] rounded-full bg-gray-700 -bottom-[30%] -left-[4%] z-30 blur-[300px]' />
+		<div className='hidden md:block absolute w-[480px] h-[480px] rounded-full bg-gray-900 -bottom-[30%] -left-[4%] z-30 blur-[300px]' />
 	)
 }
 
