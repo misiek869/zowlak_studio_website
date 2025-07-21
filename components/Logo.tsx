bbf88e8a8e44500ce60d23715f421eab955a2ac8
Michal Gawik <gawlik.zator@o2.pl>
2025-07-21 06:37:45 +0200

    center logo

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,9 +1,8 @@
 import Link from 'next/link'
 
-
 const Logo = () => {
 	return (
-		<Link href={'#'} className='text-[28px] font-normal'>
+		<Link href={'#'} className='text-[28px] font-normal self-center'>
 			Zwolak <span className='font-bold'>Studio</span>
 		</Link>
 	)
