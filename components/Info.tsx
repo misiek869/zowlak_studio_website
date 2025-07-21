78b15ab2657ce139c3dad7a32b894242f838c052
Michal Gawik <gawlik.zator@o2.pl>
2025-07-21 06:21:00 +0200

    change info order

--- components/Info.tsx
+++ components/Info.tsx
@@ -10,17 +10,18 @@ const Info = () => {
 		<div className='flex flex-col gap-6 md:gap-10'>
 			<div className='flex flex-col md:flex-row gap-4'>
 				<div className='w-[280px] flex items-start gap-4'>
+					<HiOutlineLocationMarker className='text-2xl mt-1 text-gray-900' />
+					<div className=''>
+						<p className='text-lg'>Lokalizacja</p>
+						<p>Oświęcim</p>
+					</div>
+				</div>
+
+				<div className='w-[280px] flex items-start gap-4'>
 					<HiOutlineCalendarDays className='text-2xl mt-1 text-gray-900' />
 					<div className=''>
 						<p className='text-lg'>Data założenia</p>
 						<p>2025</p>
-					</div>
-				</div>
-				<div className='w-[280px] flex items-start gap-4'>
-					<HiOutlineMail className='text-2xl mt-1 text-gray-900' />
-					<div className=''>
-						<p className='text-lg'>Email</p>
-						<p>zwolakstudio@gmail.com</p>
 					</div>
 				</div>
 			</div>
@@ -33,11 +34,12 @@ const Info = () => {
 						<p>+48 512 145 522</p>
 					</div>
 				</div>
+
 				<div className='w-[280px] flex items-start gap-4'>
-					<HiOutlineLocationMarker className='text-2xl mt-1 text-gray-900' />
+					<HiOutlineMail className='text-2xl mt-1 text-gray-900' />
 					<div className=''>
-						<p className='text-lg'>Lokalizacja</p>
-						<p>Oświęcim</p>
+						<p className='text-lg'>Email</p>
+						<p>zwolakstudio@gmail.com</p>
 					</div>
 				</div>
 			</div>
