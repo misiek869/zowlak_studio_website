0e51ce33a7261b11c8c7c8e56b9dc58c09d917e4
Michal Gawik <gawlik.zator@o2.pl>
2025-07-21 06:16:33 +0200

    change dispaly flex row from small screens

--- components/Stats.tsx
+++ components/Stats.tsx
@@ -2,7 +2,7 @@ import CountUp from 'react-countup'
 
 const Stats = () => {
 	return (
-		<div className=' flex flex-col xl:flex-row xl:items-center gap-8 xl:gap-12'>
+		<div className=' flex flex-col sm:flex-row xl:items-center gap-8 xl:gap-12'>
 			<div className='flex gap-4'>
 				<div className='text-5xl font-bold text-gray-900 flex'>
 					<CountUp end={12} duration={5} delay={2} className='block w-[60px]' />
